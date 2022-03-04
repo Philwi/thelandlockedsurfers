20001fde941fde74866a0f5deae9060264a3314b
Philipp Winkler <winkler@webit.de>
2022-03-04 19:36:25 +0100

    [TASK] add a tag section

--- src/components/blog-post/ParagraphElement.vue
+++ src/components/blog-post/ParagraphElement.vue
@@ -1,11 +1,20 @@
 <template lang="pug">
-p.pb-12 {{ text }}
+template(v-for="element in textElements")
+  template(v-if="element.elementType == 'paragraph' && !!element.text")
+    p.pb-12 {{ element.text }}
+  template(v-if="element.elementType == 'hyperlink'")
+    a.pb-12.underline.cursor-pointer.block.text-center(:href="element.href" target="_blank") {{ element.text }}
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
 export default defineComponent({
+  data() {
+    return {
+      textElements: []
+    }
+  },
   props: {
     textElement: {
       type: Object,
@@ -15,10 +24,23 @@ export default defineComponent({
       }
     }
   },
-  computed: {
-    text() {
-      return this.textElement.content[0].value
-    }
+  created() {
+    this.textElement.content.forEach(content => {
+      let element = { elementType: '', text: '', href: '' }
+      switch (content.nodeType) {
+        case 'text':
+          element['elementType'] = 'paragraph'
+          element['text'] = content.value
+        break;
+        case 'hyperlink':
+          element['elementType'] = 'hyperlink'
+          element['text'] = content.content[0].value
+          element['href'] = content.data.uri
+        }
+
+      this.textElements.push(element)
+      }
+    )
   }
 })
 </script>
