6930548f77587a604b943a75da3925c4d195df93
Philipp Winkler <winkler@webit.de>
2022-03-10 21:58:33 +0100

    [TASK] refactor possible text elements from contentful

---
+++ src/components/blog-post/text-elements/SpecialFontTextElement.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+p.pb-12.inline(:class="element.cssClass") {{ element.text }}
+</template>
+
+<script>
+export default {
+  props: {
+    element: {
+      required: true,
+      type: Object,
+      text: {
+        required: true,
+        type: String
+      },
+      cssClass: {
+        required: true,
+        type: String
+      }
+    }
+  }
+
+}
+</script>
