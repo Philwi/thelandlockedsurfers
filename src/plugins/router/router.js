41d637c438a2f337b607b87d80a5a1eb493c1630
Philipp Winkler <winkler@webit.de>
2022-03-12 12:51:25 +0100

    [FEATURE] add nice scroll behaviour for route change

--- src/plugins/router/router.js
+++ src/plugins/router/router.js
@@ -20,9 +20,11 @@ const routes = [
 ]
 
 export const router = createRouter({
-  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
   history: createWebHistory(),
-  routes, // short for `routes: routes`
+  routes,
+  scrollBehavior() {
+    document.getElementById('app').scrollIntoView({ behavior: 'smooth' });
+  }
 })
 
 router.beforeEach((to) => {
