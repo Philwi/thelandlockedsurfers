eeb76ad18106c268551cbb25f8ca9afbc27f5d3d
Philipp Winkler <winkler@webit.de>
2022-03-19 10:00:51 +0100

    [FEATURE] use ice bears instead

--- src/components/sections/WaveWithImage.vue
+++ src/components/sections/WaveWithImage.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 section.sectionSize.bg-black.h-8.pt-0.pb-0(class="lg:h-24")
   .flex.justify-center
-    img.h-128.z-10.object-contain(src="@/assets/house.png")
+    img.h-128.z-10.object-contain(src="@/assets/ice_bear.png")
 svg.bg-secondary.z-0.pb-80(xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="xl:pb-12 lg:pb-28 md:pb-64")
   path(fill="black" fill-opacity="1" d="M0,288L48,277.3C96,267,192,245,288,213.3C384,181,480,139,576,154.7C672,171,768,245,864,250.7C960,256,1056,192,1152,165.3C1248,139,1344,149,1392,154.7L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z")
 </template>
