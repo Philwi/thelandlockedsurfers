359f65ec9f591d8c812c3367e2947e5c9b3a2475
Philipp Winkler <winkler@webit.de>
2022-03-19 22:00:26 +0100

    [FEATURE] parameterize wave with image and use it again

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,21 @@
 <template lang="pug">
 HeroStage
 HowItWorks
-WaveWithImage
+WaveWithImage(
+  :imageSrc="topWaveWithImageProps.imageSrc"
+  :mainColorCssClass="topWaveWithImageProps.mainColorCssClass"
+  :secondaryColorCssClass="topWaveWithImageProps.secondaryColorCssClass"
+  :svgFillColor="topWaveWithImageProps.svgFillColor"
+  )
 Features
 WaveBlog
 Pricing
+WaveWithImage(
+  :imageSrc="bottomWaveWithImageProps.imageSrc"
+  :mainColorCssClass="bottomWaveWithImageProps.mainColorCssClass"
+  :secondaryColorCssClass="bottomWaveWithImageProps.secondaryColorCssClass"
+  :svgFillColor="bottomWaveWithImageProps.svgFillColor"
+  )
 FAQ
 </template>
 
@@ -17,8 +28,26 @@ import HowItWorks from '@/components/sections/HowItWorks.vue'
 import Pricing from '@/components/sections/Pricing.vue'
 import WaveBlog from '@/components/sections/WaveBlog.vue'
 import WaveWithImage from '@/components/sections/WaveWithImage.vue'
+import IceBear from '@/assets/ice_bear.png'
+import Waschbaer from '@/assets/waschbaer.png'
 
 export default defineComponent({
+  data() {
+    return {
+      topWaveWithImageProps: {
+        imageSrc: IceBear,
+        mainColorCssClass: 'bg-black',
+        secondaryColorCssClass: 'bg-secondary',
+        svgFillColor: 'black'
+      },
+      bottomWaveWithImageProps: {
+        imageSrc: Waschbaer,
+        mainColorCssClass: 'bg-secondary',
+        secondaryColorCssClass: 'bg-black',
+        svgFillColor: '#F4F2ED'
+      }
+    }
+  },
   components: {
     FAQ, Features, HeroStage, HowItWorks, Pricing, WaveBlog, WaveWithImage
   }
