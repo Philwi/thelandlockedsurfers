de6b495d78f0e5f4545f6660836e0097d0b6c8a3
Philipp Winkler <winkler@webit.de>
2022-03-20 13:03:42 +0100

    [TASK] add typescript to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,16 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
     'plugin:vue/base',
     'plugin:vue/vue3-essential',
     'plugin:vue/vue3-strongly-recommended',
     'plugin:vue/vue3-recommended',
-
-
+    'plugin:@typescript-eslint/recommended'
   ]
 }
